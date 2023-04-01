28c21159eb432d68190ec51f400944140047727c
Dragos <dragos.tanasa99@gmail.com>
2023-04-01 16:51:00 +0200

    push di aggiornamento

--- kernel_regression/main.py
+++ kernel_regression/main.py
@@ -6,13 +6,20 @@ from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 import misc
 
+# TODO 
+# 1) montecarlo simulation to compare the two methods
+# 2) check behaviour in homoschedacity and heteroschedacity
+# 3) corner behaviour
+
 def main():
     
-    y, x = misc.data_generating_process(1000)
+    montecalro_simulation = 100
     
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 44)
-    
-    kf = KFold(n_splits = 10)
+    for i in range(montecalro_simulation):
+        
+        y, x = misc.data_generating_process(5000)
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 44)
+        kf = KFold(n_splits = 10)
 
     # K FOLD CROSS VALIDATION
     
@@ -31,17 +38,29 @@ def main():
 
 def prova_knn():
     
-    y, x = misc.data_generating_process(1000)
+    y, x = misc.data_generating_process(5000)
     
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.33, random_state = 42)
-    knn_reg = knn.knn(x_train, y_train, 50)
-    pred = []
-    
-    for x in x_test:
-        pred.append(knn_reg(x))
+    kf = KFold(n_splits = 20)
+    neighbors = 200
+    mse = []
+    n = []
+
+    for i, (train_index, validation_index) in enumerate(kf.split(x_train)):
         
-    plt.plot(x_test, pred, ".", color = "red")
-    plt.plot(x_test, y_test, ".", color = "green")
+        #split data into train and validation
+        x_train_kf = [x_train[i] for i in train_index]
+        y_train_kf = [y_train[i] for i in train_index]
+        x_validation_kf = [x_train[i] for i in validation_index]
+        y_validation_kf = [y_train[i] for i in validation_index]
+        
+        knn_model = knn.knn(x_train_kf, y_train_kf, neighbors)
+        _, error = knn_model.predict(x_validation_kf, y_validation_kf)
+        mse.append(error)
+        n.append(neighbors)
+        neighbors = neighbors - 10
+    
+    plt.plot(n, mse, color = "blue")
     plt.show()
     
 if __name__ == "__main__":
